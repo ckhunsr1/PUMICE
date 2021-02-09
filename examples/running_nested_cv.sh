85b43efbe30bc8e88893d2f710cfaafeb14d3ede
Chachrit Khunsriraksakul <46538781+ckhunsr1@users.noreply.github.com>
2021-02-08 21:24:45 -0500

    Update running_nested_cv.sh

--- examples/running_nested_cv.sh
+++ examples/running_nested_cv.sh
@@ -4,16 +4,20 @@ file_num=$1
 met=$2 ##3d,constant##
 type=$3 ##3d:loop,tad,domain,pchic; constant:250,1000##
 
+INPUT="<PATH TO INPUT FOLDER>"
+OUTPUT="<PATH TO OUTPUT FOLDER>"
+BEDTOOLS="<PATH TO BEDTOOLS>"
+
 Rscript PUMICE.nested_cv.R \
-	--geno input/GEUVADIS_chr22_genotype_subset.traw \
+	--geno ${INPUT}/GEUVADIS_chr22_genotype_subset.traw \
 	--chr 22 \
-	--exp input/GEUVADIS_chr22_expression_subset.txt \
-	--out "<PATH TO OUTPUT FOLDER>" \
+	--exp ${INPUT}/GEUVADIS_chr22_expression_subset.txt \
+	--out ${OUTPUT} \
 	--method ${met} \
 	--type ${type} \
-	--window_path input/LCL_chr22_${type}.txt \
-	--bedtool_path "<PATH TO BEDTOOLS>" \
-	--epi_path input/ENCFF028SGJ_chr22_screen.bed \
+	--window_path ${INPUT}/LCL_chr22_${type}.txt \
+	--bedtool_path ${BEDTOOLS} \
+	--epi_path ${INPUT}/ENCFF028SGJ_chr22_screen.bed \
 	--fold 5 \
 	--total_file_num 10 \
 	--file_num ${file_num}
