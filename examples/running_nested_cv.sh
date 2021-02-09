c740297aae6006538c59c7335ea0849725a6e94b
Chachrit Khunsriraksakul <46538781+ckhunsr1@users.noreply.github.com>
2021-02-08 21:19:24 -0500

    Update running_nested_cv.sh

--- examples/running_nested_cv.sh
+++ examples/running_nested_cv.sh
@@ -5,15 +5,15 @@ met=$2 ##3d,constant##
 type=$3 ##3d:loop,tad,domain,pchic; constant:250,1000##
 
 Rscript PUMICE.nested_cv.R \
-	--geno example_input/GEUVADIS_chr22_genotype_subset.traw \
+	--geno input/GEUVADIS_chr22_genotype_subset.traw \
 	--chr 22 \
-	--exp example_input/GEUVADIS_chr22_expression_subset.txt \
+	--exp input/GEUVADIS_chr22_expression_subset.txt \
 	--out "<PATH TO OUTPUT FOLDER>" \
 	--method ${met} \
 	--type ${type} \
-	--window_path example_input/LCL_chr22_${type}.txt \
+	--window_path input/LCL_chr22_${type}.txt \
 	--bedtool_path "<PATH TO BEDTOOLS>" \
-	--epi_path example_input/ENCFF028SGJ_chr22_screen.bed \
+	--epi_path input/ENCFF028SGJ_chr22_screen.bed \
 	--fold 5 \
 	--total_file_num 10 \
 	--file_num ${file_num}
