a9d8b1b4085a52f006211a2e23e0594e563315f7
Chachrit Khunsriraksakul <46538781+ckhunsr1@users.noreply.github.com>
2023-04-21 16:16:18 -0400

    Update running_pumice+.sh

--- examples/running_pumice+.sh
+++ examples/running_pumice+.sh
@@ -8,6 +8,6 @@ Rscript PUMICE+.association_test.R \
 	--geno ${INPUT}/EUR.chr22.final \
 	--chr 22 \
 	--gwas ${INPUT}/gwas_ss_chr22.txt \
-  --pumice_weight ${INPUT}/PUMICE_Cells_EBV-transformed_lymphocytes.db \
-  --utmost_weight ${INPUT}/UTMOST_Cells_EBV-transformed_lymphocytes.db \
-  --out ${OUTPUT}
+        --pumice_weight ${INPUT}/PUMICE_Cells_EBV-transformed_lymphocytes.db \
+        --utmost_weight ${INPUT}/UTMOST_Cells_EBV-transformed_lymphocytes.db \
+        --out ${OUTPUT}
